d89a63cbe9dd4b8e1c344fc3f6e73710e8f3d25f
cornel <cornelius.jason.cj@gmail.com>
2026-04-20 03:57:06 +0700

    fix: start mithia

--- mithia/start-mithia-servers.sh
+++ mithia/start-mithia-servers.sh
@@ -39,8 +39,28 @@ echo "Char-server is up. Starting map-server..."
 
 # Start map-server
 ./map-server &
+map_server_pid=$!
 
+
+cleanup() {
+    echo "Shutting down servers..."
+
+    kill $map_server_pid
+    kill $save_server_pid
+    kill $char_server_pid
+    kill $login_server_pid
+
+    wait
+    echo "All servers stopped."
+    exit 0
+}
 # Optionally, wait for all background processes to finish
-wait $login_server_pid
-wait $char_server_pid
+# wait $login_server_pid
+# wait $char_server_pid
+
+# Tangkap Ctrl+C
+trap cleanup SIGINT
+
+# Tunggu semua process
+wait
 
